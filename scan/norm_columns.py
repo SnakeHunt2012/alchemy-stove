db53ccbd55ff07a5655bea9023eb3b25b29fb549
SnakeHunt2012 <SnakeHunt2012@gmail.com>
2016-01-11 17:03:54 +0800

    Add scan example: Computing norms of columns of X.

---
+++ scan/norm_columns.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# 
+# Scan Example: Computing norms of columns of X
+#
+
+import numpy as np
+
+from theano import config
+from theano import scan
+from theano import function
+from theano import tensor as tt
+
+x_matrix = tt.matrix('X')
+
+results, updates = scan(lambda x_i: tt.sqrt(tt.sum(x_i ** 2)), sequences = [x_matrix.T])
+norm_columns = function(inputs = [x_matrix], outputs = results)
+
+x_matrix_value = np.diag(np.arange(1, 6, dtype = config.floatX), 1)
+
+print(norm_columns(x_matrix_value))
+print(np.sqrt((x_matrix_value ** 2).sum(0)))
